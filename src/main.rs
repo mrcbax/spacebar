34ec51f5d6d653073662bf8a214e79ca88a38c1d
Diego Bustamante <diegobust4545@gmail.com>
2017-02-16 12:31:45 -0500

    Rust is now connected to PostgreSQL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
+
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::connect("postgresql://root:toor@localhost", TlsMode::None)
+            .unwrap();
+
+    conn.execute("CREATE TABLE person (
+                    id              SERIAL PRIMARY KEY,
+                    name            VARCHAR NOT NULL,
+                    data            BYTEA
+                  )", &[]).unwrap();
+    let me = Person {
+        id: 0,
+        name: "Steven".to_owned(),
+        data: None
+    };
+    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+                 &[&me.name, &me.data]).unwrap();
+
+    for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
+        let person = Person {
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2)
+        };
+        println!("Found person {}", person.name);
+    }
 }
