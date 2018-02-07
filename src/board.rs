a3dec409a6ad5fb34f77d774f864e6f32743aca0
Chad Baxter <cbaxter@mail.umw.edu>
2018-02-07 16:47:39 -0500

    fixed syntax error

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
 //clipboard code
 use clipboard::*;
 
-pub fn parse_clipboard(String: spacebar) {
+pub fn parse_clipboard(spacebar: String) {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     ctx.set_contents(spacebar.as_str().to_owned()).unwrap();
 }
